895bc1ca0a2dc8db09b88307692d1e34616a6722
ronsig30 <ronaldrogony30@gmail.com>
2024-08-01 01:13:36 +0300

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,7 +6,6 @@ def canUnlockAll(boxes):
     # Initialize a queue for BFS and a set for visited boxes
     queue = deque([0])
     visited = set([0])
-    
     # Perform BFS
     while queue:
         current_box = queue.popleft()
@@ -14,6 +13,4 @@ def canUnlockAll(boxes):
             if key not in visited and key < len(boxes):
                 visited.add(key)
                 queue.append(key)
-    
-    # Check if all boxes have been visited
     return len(visited) == len(boxes)
