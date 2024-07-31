ee001d5e561ae8753a4557aa5f4f057cae148ee4
ronsig30 <ronaldrogony30@gmail.com>
2024-08-01 00:08:19 +0300

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,31 +1,25 @@
 #!/usr/bin/python3
 
-def canUnlockAll(boxes):
-    """
-    Determines if all the boxes can be unlocked.
+total_boxes = len(boxes)
 
-    Args:
-        boxes (list of lists): A list where each sublist contains keys to other
-        boxes.
+setofkeys = [0]
 
-    Returns:
-        bool: True if all boxes can be opened, False otherwise.
-    """
-    if not boxes:
-        return False
+counter = 0
 
-    # Initialize visited set and queue
-    visited = set()
-    queue = [0]  # Start with the first box
+index = 0
 
-    while queue:
-        box = queue.pop(0)
-        if box not in visited:
-            visited.add(box)
-            # Add all keys (boxes) we can reach from the current box
-            for key in boxes[box]:
-                if key < len(boxes) and key not in visited:
-                    queue.append(key)
+while index < len(setofkeys):
 
-    # Check if we visited all boxes
-    return len(visited) == len(boxes)
+    setkey = setofkeys[index]
+
+    for key in boxes[setkey]:
+
+        if 0 < key < total_boxes and key not in setofkeys:
+
+            setofkeys.append(key)
+
+            counter += 1
+
+    index += 1
+
+return counter == total_boxes - 1
