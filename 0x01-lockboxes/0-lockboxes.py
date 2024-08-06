5f3a28bf3bc381c285ed4d2898f4188c5479d7d2
ronsig30 <ronaldrogony30@gmail.com>
2024-08-06 19:02:41 +0300

    Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,16 +1,40 @@
 #!/usr/bin/python3
+"""
+This module contains a function to determine if all boxes can be unlocked.
+"""
 
-def canUnlockAll(boxes):
-    from collections import deque
+from collections import deque
+from typing import List
 
-    # Initialize a queue for BFS and a set for visited boxes
+
+def canUnlockAll(boxes: List[List[int]]) -> bool:
+    """
+    Determine if all boxes can be unlocked.
+
+    Args:
+        boxes (List[List[int]]): A list where each element is a list of keys av
+        ailable in that box.
+
+    Returns:
+        bool: True if all boxes can be unlocked, False otherwise.
+    """
+    n = len(boxes)
+    if n == 0:
+        return False
+
+    # Initialize BFS
+    visited = [False] * n
     queue = deque([0])
-    visited = set([0])
-    # Perform BFS
+    visited[0] = True
+
     while queue:
-        current_box = queue.popleft()
-        for key in boxes[current_box]:
-            if key not in visited and key < len(boxes):
-                visited.add(key)
+        box_index = queue.popleft()
+
+        # Access keys from the current box
+        for key in boxes[box_index]:
+            if 0 <= key < n and not visited[key]:
+                visited[key] = True
                 queue.append(key)
-    return len(visited) == len(boxes)
+
+    # Check if all boxes have been visited
+    return all(visited)
